1c5ff07bb6967e32be4def638d5b4dd75015abb9
Harshmneet Arora <harshmeet_arora@yahoo.com>
2018-10-23 20:12:16 -0700

    server

--- server.js
+++ server.js
@@ -3,19 +3,17 @@ const bodyparser = require('body-parser');
 const exphbs = require('express-handlebars');
 
 
-var port = 3000;
-var url ="http://localhost:3000";
+var port = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/', (req,res) => {
-    console.log("hello world");
-    res.send({message : "Hello"});
+    res.send("Hello");
 });
 
-app.post('/users', function(req, res) {
-    console.log("body", req.body);
-});
+// app.post('/users', function(req, res) {
+//     console.log("body", req.body);
+// });
 
 
 app.listen(port, () => console.log("SERVER STARTED..."));
