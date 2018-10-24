76d0e0ab0beb46929661d700845c72ea73418ab4
Harshmneet Arora <harshmeet_arora@yahoo.com>
2018-10-24 11:02:57 -0700

    hashId

--- server.js
+++ server.js
@@ -1,19 +1,78 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const exphbs = require('express-handlebars');
+const model = require('./Database/db.js');
+var Hashids = require('hashids');
+var hashids = new Hashids();
 
+
+
+const testClientHarsh = {
+	name: "Harsh",
+	id : 7,
+	age: "22",
+}
 
 var port = process.env.PORT || 3000;
 
 const app = express();
 
+
 app.get('/test', (req,res) => {
     res.send("Hello");
 });
 
-// app.post('/users', function(req, res) {
-//     console.log("body", req.body);
+
+
+
+
+
+
+// model.callDB(model.addNewClient, testClientHarsh)
+// .then(() => {
+// console.log ("OOOOOOOOO");
+// res.send("Hello");
 // });
 
 
+// model.callDB(model.addNewClient, testClientHarsh)
+// .then(() => {
+// console.log ("OOOOOOOOO");
+// res.send("Hello");
+// });
+
+
+
+
+
+
+app.get('/idHash', (req,res) => {
+    var fbID = req.id;
+    //var fbID = 13264652373;
+    var hashId = hashids.encode(fbID);
+    console.log(hashId);
+    res.send({
+        id: hashId
+    });
+});
+
+
+
+
+
+
+// app.get('/addUser', (req,res) => {
+
+//     //var user = req.user;
+//     console.log("db");
+//     model.callDB(model.addNewClient, testClientHarsh)
+//     .then(() => {
+//     console.log ("OOOOOOOOO");
+//     res.send("Hello");
+//     });
+// });
+
+// app.post('/users', function(req, res) {
+//     console.log("body", req.body);
+// });
 app.listen(port, () => console.log("SERVER STARTED..."));
