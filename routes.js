af5a16efd81a5a15391f0f66904cd3a5ce414f52
Harshmneet Arora <harshmeet_arora@yahoo.com>
2018-11-01 15:26:08 -0700

    heroku routes

--- routes.js
+++ routes.js
@@ -1,33 +1,33 @@
 let express = require('express');
 let router  = express.Router();
-
-const model = require('./Database/db.js');
+let model = require('./Database/db.js');
 
 
 //test 
 router.route('/test')
     .get((req,res) => {
-    res.send("Hello");
+        console.log("check");
+        res.send("Hello");
 });
 
 //hash id
 router.route('/idHash')
     .post((req,res) => {
-    var fbID = req.body.id;
-    var hashId = hashids.encode(fbID);
-    console.log("hashId: " + hashId);
-    res.send(JSON.stringify({
-        'id' : hashId
+        var fbID = req.body.id;
+        var hashId = hashids.encode(fbID);
+        console.log("hashId: " + hashId);
+        res.send(JSON.stringify({
+            'id' : hashId
     }));
 });
 
 //add user
 router.route('/addUser')
-  .post((req,res) => {
-    let user = req.user;
-    console.log("db");
-    model.addNewClient(testClientHarsh);
-    res.send("user added");
+    .post((req,res) => {
+        let user = req.user;
+        console.log("db");
+        model.addNewClient(testClientHarsh);
+        res.send("user added");
 });
 
 module.exports = router;
